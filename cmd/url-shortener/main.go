b0c03fbb198ca706102ee0c7995d34567bdfd133
Stefan Zha <stephenzhavrid@gmail.com>
2025-07-17 12:22:36 +0300

    logger with slog was implemented

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/stevenstr/url-shortener/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: init config: cleanenv
+	// Done: init config: cleanenv
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog (log/slog)
+	// Done: init logger: slog (log/slog)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener...", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled...")
 
 	// TODO: init  storage: sqlite
 
@@ -16,4 +29,28 @@ func main() {
 
 	// TODO: run server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		// текстовый
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		// текстовый
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		// а вот тут уже json для grafana kibana
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
