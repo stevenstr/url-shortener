61123a6fa8b38efee7519f9f0fdb789867e95ec6
Stefan Zha <stephenzhavrid@gmail.com>
2025-07-17 09:22:27 +0300

    go structures for configs

---
+++ internal/config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import "time"
+
+type Config struct {
+	// required для защиты от запуска без типа среды
+	Env         string `yaml:"env" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"10s"`
+}
