ed67f25d7d6be40a099af27526e502e8e949393f
Stefan Zha <stephenzhavrid@gmail.com>
2025-07-17 11:24:27 +0300

    config with cleanenv was resolved

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	// required для защиты от запуска без типа среды
@@ -13,4 +19,26 @@ type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"10s"`
+}
+
+func MustLoad() *Config {
+	// get name
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check if file exests
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config file does not exist: %w", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatal("cannot read config file: %w", configPath)
+	}
+
+	return &cfg
 }
